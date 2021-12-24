e87b6ed69e860b0eb673d47fc93b7d88dd34fdf5
AndriiBozh <deux_jours_avant@hotmail.com>
2021-12-24 10:08:33 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const firstDigitInputForm = document.getElementById('first-digit');
+const secondDigitInputForm = document.getElementById('second-digit');
+const selectEl = document.getElementById('math-operators')
+const calculateBtn = document.getElementById('calculate-btn')
+const display = document.getElementById('result')
+
+let result = 0;
+
+function calculate() {
+
+    let firstNumber = Number(firstDigitInputForm.value);
+    let secondNumber = Number(secondDigitInputForm.value)
+    let mathOperand = selectEl.value;    
+    
+    if (mathOperand === '+') {        
+        result = firstNumber + secondNumber;
+    }
+
+    if (mathOperand === '-') {
+        result = firstNumber - secondNumber;      
+    }
+
+    if (mathOperand === '*') {
+        result = firstNumber * secondNumber;       
+    }
+
+    if (mathOperand === '/') {
+        result = firstNumber / secondNumber;        
+    }    
+   
+    result = Math.trunc(result)
+    return result; 
+} 
+
+function displayResult() {
+    display.innerHTML = result;
+}
+
+firstDigitInputForm.addEventListener('change', calculate);
+secondDigitInputForm.addEventListener('change', calculate)
+selectEl.addEventListener('change', calculate)
+calculateBtn.addEventListener('click', displayResult)
